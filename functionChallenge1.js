23061dee0089e7b4018d5f2c6495a2067913181c
macbook pro <macbookpro@Dianas-MacBook-Pro.local>
2024-06-12 11:13:55 +0200

    first challenge solved

---
+++ functionChallenge1.js
@@ -0,0 +1,30 @@
+
+//create an arrow function for calculating the average of scores
+
+const calcAverage = (marks1, marks2, marks3) => {
+    const addMarks = marks1 + marks2 + marks3;
+    const result = addMarks / 3;
+    return result;
+}
+console.log(calcAverage(20, 15, 18));
+
+//creating the variables marksdophines&marksKoalas and assign calcaverage to it!
+
+const marksDophines = calcAverage(30, 20, 8);
+const marksKoalas = calcAverage(50, 5, 10);
+
+const CheckWinner = function calcWinner(marksDophines, marksKoalas) {
+    if(marksDophines > marksKoalas) {
+        console.log(`marksDophines is the Winner with the mark of ${marksDophines}`);
+    } else if (marksDophines < marksKoalas){
+        console.log(`markKoalas is the Winner with the mark of ${marksKoalas}`);
+    } else {
+        console.log("No Teams Wins!!!");
+    }
+}
+
+const winnerResult = CheckWinner(marksDophines, marksKoalas);
+console.log(winnerResult);
+
+
+
