8e411ee82df0d3bdd07234103029b26a762cbe76
Kevin Iradukunda <kevin.iradukunda@amalitechtraining.org>
2024-06-12 12:57:02 +0200

    the ArrayChallenge project solved

---
+++ Week0/arrayChallenge.js
@@ -0,0 +1,30 @@
+//Creating the calcTip function
+const calcTip = function Tip(bill) {
+    const calcBill1 = bill * 15;
+    const calcBill2 = bill * 20;
+    calcpercentange1 = calcBill1 /100;
+    calcpercentange2 = calcBill2 /100;
+    if(bill >= 50 && bill <= 300) {
+        return calcpercentange1;
+    } else {
+        return calcpercentange2;
+    }
+
+}
+
+
+let bills = [20,130,60];
+const tips = [];
+let addTotips = tips.push(calcTip(bills[0]));
+addTotips = tips.push(calcTip(bills[1]));
+addTotips = tips.push(calcTip(bills[2]));
+const newTipsArray = addTotips;
+
+console.log(newTipsArray);
+
+const total = [];
+  let sum = total.push(bills[0] + newTipsArray[0]);
+  sum = total.push(bills[1] + newTipsArray[1]);
+  sum = total.push(bills[2] + newTipsArray[2]);
+
+  console.log(sum);
